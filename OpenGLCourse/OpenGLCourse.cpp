6fdaf00520e4fbc2f7260603f37a690169662253
Wiktor-wewe <wewersajtys.wiktor@gmail.com>
2023-11-23 12:54:52 +0100

    lesson: hello window

--- OpenGLCourse/OpenGLCourse.cpp
+++ OpenGLCourse/OpenGLCourse.cpp
@@ -1,6 +1,18 @@
-﻿#include <glad/glad.h>
+﻿#include <iostream>
+#include "glad.h"
 #include <GLFW/glfw3.h>
-#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
 
 int main()
 {
@@ -18,5 +30,42 @@ int main()
     }
     glfwMakeContextCurrent(window);
     
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    float r = 0.0f;
+    float g = 0.0f;
+    float b = 0.0f;
+    
+    while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+
+        // rendering here
+        r += 0.007f;
+        g += 0.002f;
+        b += 0.001f;
+        glClearColor(r, g, b, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        if (r > 1.0f) {
+            r = 0.0f;
+        }
+        if (g > 1.0f) {
+            g = 0.0f;
+        }
+        if (b > 1.0f) {
+            b = 0.0f;
+        }
+
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
